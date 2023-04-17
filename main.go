90f310fde96db1f35e5016524b565bfbc4331b68
davidbystrom <dbys@kth.se>
2023-04-17 16:47:32 +0200

    Finished Hashchecking program

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello World")
+  f, err := os.Open(os.Args[1])
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer f.Close()
+
+  h := sha256.New()
+  if _, err := io.Copy(h, f); err != nil {
+    log.Fatal(err)
+  }
+
+
+  hash := hex.EncodeToString(h.Sum(nil))
+
+  fmt.Println("Generated hash from file: ", hash)
+  fmt.Println("Hash collected from input:", strings.ToLower(os.Args[2]))
+  fmt.Println("Hashes match:", string(hash) == strings.ToLower(os.Args[2]))
 }
